92abcea132d6521fa1d63a7b2391eaffdcb11f88
Jan Kedra <aniemuse@gmail.com>
2021-12-17 01:07:38 +0100

    Imp. of active layer

---
+++ active_layer.py
@@ -0,0 +1,27 @@
+from layer import Layer
+
+# inherit from base class Layer
+class ActiveLayer(Layer):
+    """
+    Represents "thinking" layer
+    """
+    def __init__(self, activation, activation_prime):
+        """
+        Sets function used to activate neurons for this layer
+        """
+        self.activation = activation
+        self.activation_prime = activation_prime
+
+    def forward_propagation(self, input_data):
+        """
+        returns the activated input
+        """
+        self.input = input_data
+        self.output = self.activation(self.input)
+        return self.output
+
+    def backward_propagation(self, output_error, learning_rate):
+        """
+        Returns input_error dE/dX for a given output_error = dE/dY.
+        """
+        return self.activation_prime(self.input) * output_error
