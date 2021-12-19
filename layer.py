8027e9b00a2238c8a707bc5ac27004748ef5e5a3
Jan Kedra <aniemuse@gmail.com>
2021-12-19 16:42:34 +0100

    Rewritten layer class

--- layer.py
+++ layer.py
@@ -1,23 +1,3 @@
-# class Layer:
-#     """
-#     Abstract class representing layer for neural network
-#     """
-#     def __init__(self) -> None:
-#         self.input = None
-#         self.output = None
-
-#     def forward_propagation(self, input):
-#         """
-#         Used to get data from previous layer
-#         """
-#         raise NotImplementedError
-
-#     def bacwards_propagation(self, output_error, learning_rate):
-#         """
-#         Needed to provide derrivative of an error based on input
-#         """
-#         raise NotImplementedError
-
 from typing import TypeVar
 from abc import ABC, abstractmethod
 T = TypeVar('T')
